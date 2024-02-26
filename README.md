4f060983767b94fb2193d17658bc2a5e8420d212
Selman <selman.ciftci.coding@gmail.com>
2024-02-26 15:04:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Vite Template with Shadcn
+
+## Purpose
+
+This vite template purpose is to save time, shadcn is already configured, if you want to re-config, delete `components.json`, `components.json` and `/lib` first
+then type following command into your terminal: `npx shadcn-ui@latest init`
+
+More Information can be found here:  ![Vite shadcn/ui]("https://ui.shadcn.com/docs/installation/vite")
+
+---
+
+## Tech
+
+- Vite React
+   - Babel
+   - ESlint
+   - TypeScript    
+- SASS
+- Tailwind
+- shadcn
+
+---
+
+## Config
+
+The necessary config code for shadcn/ui is already inserted in their right places.
+The types are also available, in case you want to work with **TypeScript**
+An `.prettierrc` file with default configs is included.
+
+---
+
+## Installation:
+
+`npm install` should suffice and you can begin to code.
+
+---
+
+Enjoy.
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+    return (
+        <>
+            <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+                <header className="text-4xl font-bold mb-8">
+                    Welcome to Vite React + TypeScript + Tailwind + Shadcn
+                </header>
+                <main className="text-lg text-center">
+                    <p>
+                        Congratulations! You've successfully set up a project
+                        with Vite, React, TypeScript, Tailwind CSS, and Shadow
+                        DOM styling using Shadcn.
+                    </p>
+                    <p className="mt-4">
+                        Get started by exploring the code in the{' '}
+                        <code>src</code> directory and making it your own.
+                    </p>
+                </main>
+                <footer className="mt-8 text-sm text-gray-500">
+                    <p>
+                        Happy coding with Vite, React, TypeScript, Tailwind, and
+                        Shadcn!
+                    </p>
+                </footer>
+            </div>
+        </>
+    )
+}
+
+export default App
