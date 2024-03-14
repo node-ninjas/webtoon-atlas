20316a7f62153736a0c0a30afb43520fa6d791bb
Patricia <125980485+loulevin@users.noreply.github.com>
2024-03-14 15:21:22 +0100

    Patricia: dark mode works

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
     return (
-        <nav>
+        <nav className=''>
             <NavLink to="/" className="m-3">Home</NavLink>
             <NavLink to="/info" className="m-3">Info</NavLink>
         </nav>
