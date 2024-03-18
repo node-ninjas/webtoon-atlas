8d4db6e1e26bd0bd0034a64a328c0ca23e87f7ab
loulevin <patricia.balbier@gmail.com>
2024-03-18 11:31:38 +0100

    Add ClerkLogin component to Header

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,11 +1,13 @@
 import { Nav } from './Nav'
 import { ModeToggle } from '../ui/mode-toggle'
+import { ClerkLogin } from './ClerkLogin'
 
 export const Header = () => {
     return (
         <>
             <div className="m-4 flex justify-between">
                 <h1 className="text-2xl">Webtoon-Atlas</h1>
+                <ClerkLogin />
                 <ModeToggle />
             </div>
             <Nav />
