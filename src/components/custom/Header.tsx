02de525009a384e3dfe15e9d11e30538a5605695
Selman <selman.ciftci.coding@gmail.com>
2024-03-18 11:49:34 +0100

    Selman: Update header
    * updated header to wrapper component
    
    * Add ClerkLogin component to Header
    
    * Update ClerkLogin import path

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,13 +1,17 @@
 import { Nav } from './Nav'
 import { ModeToggle } from '../ui/mode-toggle'
-import { ClerkLogin } from './ClerkLogin'
 
-export const Header = () => {
+type props = {
+    children: React.ReactNode
+}
+
+export const Header = ({ children }: props) => {
     return (
         <>
             <div className="m-4 flex justify-between">
                 <h1 className="text-2xl">Webtoon-Atlas</h1>
-                <ClerkLogin />
+                {children}
+
                 <ModeToggle />
             </div>
             <Nav />
