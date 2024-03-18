6cdbfa656fb28354efd43e85ea527fe8788b1243
Selman <selman.ciftci.coding@gmail.com>
2024-03-18 11:48:59 +0100

    Selman: Implement clerk
    * installed clerk
    
    * added update script for safety
    
    * Refactor main.tsx to use ClerkProvider and add error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+import './index.scss'
+import { RouterProvider } from 'react-router-dom'
+import router from './AppRouter.tsx'
 
-import "./index.scss";
-import { RouterProvider } from "react-router-dom";
-import router from "./AppRouter.tsx";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+    </ClerkProvider>
+)
