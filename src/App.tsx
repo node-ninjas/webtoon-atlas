e96b7db7c2819a1f52556cc716055978758a8e98
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-18 11:42:07 +0100

    updated header to wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Header />
+                <Header> </Header>
                 <main className="m-5">
                     <Outlet />
                 </main>
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -2,11 +2,16 @@ import { Nav } from './Nav'
 import { ModeToggle } from '../ui/mode-toggle'
 import { ClerkLogin } from './ClerkLogin'
 
-export const Header = () => {
+type props = {
+    children: React.ReactNode
+}
+
+export const Header = ({ children }: props) => {
     return (
         <>
             <div className="m-4 flex justify-between">
                 <h1 className="text-2xl">Webtoon-Atlas</h1>
+                {children}
                 <ClerkLogin />
                 <ModeToggle />
             </div>
