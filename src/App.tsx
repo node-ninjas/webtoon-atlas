527915c6b750b682f1ee9e01bfdfe25b9c6d7f33
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-18 11:47:19 +0100

    Update ClerkLogin import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/custom/Header'
 import { ThemeProvider } from './components/ui/theme-provider'
-import { ClerkLogin } from './components/custom/ClerkLogin'
+import { ClerkLogin } from '@/components/custom/ClerkLogin'
 
 function App() {
     return (
         <>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                 <Header>
-                    <ClerkLogin />{' '}
+                    <ClerkLogin />
                 </Header>
                 <main className="m-5">
                     <Outlet />
