5dfb0bf416a7331afe230fce3342642297313fd6
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-18 11:43:06 +0100

    Add ClerkLogin component to Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/custom/Header'
 import { ThemeProvider } from './components/ui/theme-provider'
+import { ClerkLogin } from './components/custom/ClerkLogin'
 
 function App() {
     return (
         <>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Header> </Header>
+                <Header>
+                    <ClerkLogin />{' '}
+                </Header>
                 <main className="m-5">
                     <Outlet />
                 </main>
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,6 +1,5 @@
 import { Nav } from './Nav'
 import { ModeToggle } from '../ui/mode-toggle'
-import { ClerkLogin } from './ClerkLogin'
 
 type props = {
     children: React.ReactNode
@@ -12,7 +11,7 @@ export const Header = ({ children }: props) => {
             <div className="m-4 flex justify-between">
                 <h1 className="text-2xl">Webtoon-Atlas</h1>
                 {children}
-                <ClerkLogin />
+
                 <ModeToggle />
             </div>
             <Nav />
