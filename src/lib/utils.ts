99e68fdc89edd3d847e6afa58813454f55703c7d
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-19 11:56:48 +0100

    Add fetchSingleEndpoint function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,19 @@ import axios from 'axios'
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const URL = 'localhost:3550'
+
+export const fetchSingleEndpoint = async (endpoint: string) => {
+    if (!endpoint) {
+        throw new Error('Please enter a correct URL with a endpoint')
+    }
+    try {
+        const response = await axios.get(`http://${URL}/${endpoint}`)
+        const data = response.data
+        console.log(data)
+        return data
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}
