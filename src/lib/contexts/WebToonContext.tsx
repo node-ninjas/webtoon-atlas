ad891ccf71115f5d4d5924f6b25acb3885b51378
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-19 17:12:17 +0100

    Refactor WebToonContext component

--- src/lib/contexts/WebToonContext.tsx
+++ src/lib/contexts/WebToonContext.tsx
@@ -1,33 +1,28 @@
 import { createContext, useState } from 'react'
 
 type authorsType = {
-  name: string,
-  location: string,
-  description: string
+    name: string
+    location: string
+    description: string
 }
-
 type webtoonTypes = {
- authors: authorsType[]
-setAuthors: React.Dispatch<React.SetStateAction<authorsType[]>>
+    authors: authorsType[]
+    setAuthors: React.Dispatch<React.SetStateAction<authorsType[]>>
 }
 
-
-export const contextData: webtoonTypes =  {
+export const contextData: webtoonTypes = {
     authors: [],
-    setAuthors: () => {} 
+    setAuthors: () => {},
 }
-
-
-
 
 type ProviderProps = {
-        children: React.ReactNode;
+    children: React.ReactNode
 }
 
 export const WebtoonContext = createContext<webtoonTypes>(contextData)
 
 export const WebtoonProvider: React.FC<ProviderProps> = ({ children }) => {
-    const [authors, setAuthors] = useState<authorsType[]>([]); 
+    const [authors, setAuthors] = useState<authorsType[]>([])
 
     const contextValue: webtoonTypes = {
         authors,
@@ -36,7 +31,7 @@ export const WebtoonProvider: React.FC<ProviderProps> = ({ children }) => {
 
     return (
         <WebtoonContext.Provider value={contextValue}>
-             {children}
+            {children}
         </WebtoonContext.Provider>
-    );
+    )
 }
