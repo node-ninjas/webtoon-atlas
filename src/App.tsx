7fd56f6e5e4abe13774d58a7bf0cfbbc3a27217e
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-19 13:16:50 +0100

    Add WebtoonProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components/custom/Header'
 import { ThemeProvider } from './components/ui/theme-provider'
+import { WebtoonProvider } from './lib/contexts/WebToonContext'
 
 function App() {
     return (
         <>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Header />
-                <main className="m-5">
-                    <Outlet />
-                </main>
-            </ThemeProvider>
+            <WebtoonProvider>
+                <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                    <Header />
+                    <main className="m-5">
+                        <Outlet />
+                    </main>
+                </ThemeProvider>
+            </WebtoonProvider>
         </>
     )
 }
--- src/lib/contexts/WebToonContext.ts
+++ src/lib/contexts/WebToonContext.ts
@@ -1,32 +1,42 @@
-import { createContext, useContext } from 'react'
+import { createContext, useState } from 'react'
+
+type authorsType = {
+  name: string,
+  location: string,
+  description: string
+}
 
 type webtoonTypes = {
-
-
+ authors: authorsType[]
+setAuthors: React.Dispatch<React.SetStateAction<authorsType[]>>
 }
 
 
-const webtoonContext: webtoonTypes =  {
-
-
-
+export const contextData: webtoonTypes =  {
+    authors: [],
+    setAuthors: () => {} 
 }
 
 
-const createWebtoonContext = createContext(webtoonContext)
 
-type providerProps = {
-    children: React.ReactNode
+
+type ProviderProps = {
+        children: React.ReactNode;
 }
 
-const WebtoonProvider = ({children}: providerProps) => {
+export const WebtoonContext = createContext<webtoonTypes>(contextData)
 
+export const WebtoonProvider: React.FC<ProviderProps> = ({ children }) => {
+    const [authors, setAuthors] = useState<authorsType[]>([]); 
 
+    const contextValue: webtoonTypes = {
+        authors,
+        setAuthors,
+    }
 
-return (
-    <createWebtoonContext.Provider value={contextValue}>
-    {children}
-    </createWebtoonContext.Provider>
-    )
-
+    return (
+        <WebtoonContext.Provider value={contextValue}>
+             {children}
+        </WebtoonContext.Provider>
+    );
 }
