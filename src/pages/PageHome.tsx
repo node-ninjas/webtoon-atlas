ebd6825906390bb77dcde7a1437a55a4cf847ed3
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-24 15:29:11 +0100

    Refactor author list rendering in PageHome component

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -3,16 +3,14 @@ import { useAppContext } from '@/lib/contexts/WebToonContext'
 export const PageHome = () => {
     const { authors } = useAppContext()
     return (
-        <>
-            <section>
-                {authors.map((author) => (
-                    <>
-                        <li>Name: {author.name}</li>
-                        <li>Location: {author.location}</li>
-                        <li>Description: {author.description}</li>
-                    </>
-                ))}
-            </section>
-        </>
+        <section>
+            {authors.map((author) => (
+                <ul key={author.name}>
+                    <li>Name: {author.name}</li>
+                    <li>Location: {author.location}</li>
+                    <li>Description: {author.description}</li>
+                </ul>
+            ))}
+        </section>
     )
 }
