5788bef9b2174939b542b39a2cd6d47e9b03b3e6
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-03-28 13:10:07 +0100

    moved variable to its intended place

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -9,12 +9,12 @@ const statusArray = [
 
 const typeArray = ['manhwa', 'manhua', 'webtoon'] as const
 
+const languageArray = ['German', 'Korean', 'Chinese', 'English'] as const
+
 // regex variables
 const priceValueRegex = /^\d+,\d{2}$/
 
 const currencySymbolArray = ['€', '$', '₩'] as const
-
-const languageArray = ['German', 'Korean', 'Chinese', 'English'] as const
 
 const isbnRegex = /^\d{13}$/
 
